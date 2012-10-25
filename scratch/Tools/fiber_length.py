9c1f6f9f9438f7639649de1c7bf4f795be05c379
acki <acki@134.130.81.225>
2012-10-25 17:56:39 +0200

    recovery

---
+++ scratch/Tools/fiber_length.py
@@ -0,0 +1,15 @@
+tau = 18.
+l = 20.
+sigma_max = 1450.
+
+def fiber_r(tau, l, sigma_max):
+    return l * tau / sigma_max
+
+print fiber_r(tau, l, sigma_max)
+
+a0 = 3000.
+i = 145.
+a = 1500.
+
+ann = 12 * i
+i_eff = (1 + i) ** n * i / ((1 + i) ** n - 1)
